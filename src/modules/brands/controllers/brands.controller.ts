9d27a5d50a17c3a2fc03d643b8252f9b31c28caf
Nerling Hernandez <nerlingfhg@gmail.com>
2026-05-13 21:12:30 -0600

    Agregar la la logica del modulo models y relacion con brands

--- src/modules/brands/controllers/brands.controller.ts
+++ src/modules/brands/controllers/brands.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { 
+    Body, 
+    Controller, 
+    Delete, 
+    Get, 
+    Param, 
+    ParseIntPipe, 
+    Patch, 
+    Post 
+} from '@nestjs/common';
 import { BrandsService } from '../services/brands.service';
-import { CreateBrandDto } from '../dto/brand.dto';
+import { CreateBrandDto, UpdateBrandDto } from '../dto/brand.dto';
 import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Brands')
@@ -22,4 +31,17 @@ export class BrandsController {
     findOne(@Param('id', ParseIntPipe) id: number) {
         return this.brandsService.findOne(id);
     }
+
+    // Patch se usa para actualizar parcialmente un recurso, mientras que Put se usa para reemplazar completamente un recurso. En este caso, como solo queremos actualizar algunos campos de la marca, es más apropiado usar Patch.
+    @Patch(':id')
+    update(@Param('id', ParseIntPipe) id: number,
+    @Body() updateBrandDto: UpdateBrandDto) {
+        return this.brandsService.update(id, updateBrandDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id', ParseIntPipe) id: number) {
+        return this.brandsService.remove(id);
+    }
+
 }
--- src/modules/brands/services/brands.service.ts
+++ src/modules/brands/services/brands.service.ts
@@ -2,7 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Brand } from '../entities/brand.entity';
 import { Repository } from 'typeorm';
-import { CreateBrandDto } from '../dto/brand.dto';
+import { CreateBrandDto, UpdateBrandDto } from '../dto/brand.dto';
 
 @Injectable()
 export class BrandsService {
@@ -28,7 +28,16 @@ export class BrandsService {
         return brand;
     }
 
-    async update() {}
+    async update(id: number, updateBrandDto: UpdateBrandDto): Promise<Brand> {
+        const brand = await this.findOne(id);
+        Object.assign(brand, updateBrandDto);
+        return await this.brandRepository.save(brand);
+    }
 
-    async remove() {}
+    // void se usa para indicar que la función no devuelve ningún valor. En este caso, como la función remove no devuelve ningún valor, es apropiado usar void.
+    // Se cambiara el void a futuro porque necesitaremos un registro
+    async remove(id: number): Promise<void> {
+        const brand = await this.findOne(id);
+        await this.brandRepository.remove(brand);
+    }
 }
